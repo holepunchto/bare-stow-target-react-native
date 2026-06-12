6b376de5ef3a870579fa31455149e49a0268106b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-12 18:56:44 +0200

    Regenerate snapshot

--- fixtures/test.snapshot.cjs
+++ fixtures/test.snapshot.cjs
@@ -20,7 +20,7 @@ export default {
 `
 
 exports['generate - types - 0'] = `declare const harness: {
-  start(opts?: object): Promise<{ ipc: import('bare-stow/host').IPC }>
+  start(opts?: import('react-native-bare-kit').WorkletOptions): Promise<{ ipc: import('bare-stow/host').IPC }>
 }
 
 export default harness
@@ -53,7 +53,7 @@ export default {
 `
 
 exports['generate with client - types - 0'] = `declare const harness: {
-  start(opts?: object): Promise<{ ipc: import('bare-stow/host').IPC; rpc: import('bare-rpc') }>
+  start(opts?: import('react-native-bare-kit').WorkletOptions): Promise<{ ipc: import('bare-stow/host').IPC; rpc: import('bare-rpc') }>
 }
 
 export default harness
